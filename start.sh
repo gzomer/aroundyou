ee50a68ca6e7e3396e0951afa41784a10e8166d7
Gustavo Zomer <gustavozomer@gmail.com>
2021-06-14 17:18:29 +0100

    Add start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+source env/bin/activate
+
+pm2 start objects.py --interpreter python3 --name objects
+pm2 start clothing.py --interpreter python3 --name clothing
+pm2 start explore.py --interpreter python3 --name explore
+pm2 start ocr.py --interpreter python3 --name ocr
+
+cd socket-image-server 
+pm2 start server.js --name socket-image
+cd ..
+
+cd socket-audio-server 
+pm2 start server.js --name socket-audio
+cd ..
